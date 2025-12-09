4aaf69f0cdb104a32d5c5d7191c5ff4dcf978641
PikaNoob <87725268+PikaNoob@users.noreply.github.com>
2025-12-09 05:14:54 +0000

    begin to clean header
    the way im doing this is absolutely ridiculous (using exodus output while looking at ida pro) but oh well

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -5,13 +5,10 @@ call build %1
 
 REM  // run fc
 echo -------------------------------------------------------------
-IF EXIST beavisbuilt.bin ( fc /b beavisbuilt.bin butthead.bin
+IF EXIST beavisbuilt.bin ( fc /b beavisbuilt.bin beavis.bin
 ) ELSE echo beavisbuilt.bin does not exist, probably due to an assembly error
 
 REM // clean up after us
-IF EXIST beavis.lst del beavis.lst
-IF EXIST beavisbuilt.bin del beavisbuilt.bin
-IF EXIST errors.txt del errors.txt
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
 pause
